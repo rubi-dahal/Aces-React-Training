891fea93b55b3354f99f831439237a1d41a50cfc
rubi-dahal <rrubidahal7@gmail.com>
2025-07-20 23:29:06 +0545

    m

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -61,7 +61,7 @@ const Hero = () => {
               </Link>
             </div>
             <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-              <Link to="/Signin" className="text-sm/6 font-semibold text-white">
+              <Link to="/signin" className="text-sm/6 font-semibold text-white">
                 Log in <span aria-hidden="true">→</span>
               </Link>
             </div>
@@ -169,12 +169,12 @@ const Hero = () => {
                 favorite blog.
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
-                <a
-                  href="/Signin"
+                <Link
+                  to="/signin"
                   className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Get started
-                </a>
+                </Link>
                 <a href="#" className="text-sm/6 font-semibold text-gray-100">
                   Learn more <span aria-hidden="true">→</span>
                 </a>
